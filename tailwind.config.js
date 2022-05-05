9a796287f2ab1b4ede4dde25ab73e3cdff40f30c
NhanHoang09 <hoangthanhnhan.17@gmail.com>
2022-05-05 09:56:05 +0700

    angular app

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./src/**/*.{html,ts}', './projects/**/*.{html,ts}'],
+  theme: {
+    borderRadius: {
+      searchRadius: '92px',
+      buttonRadius: '48px',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
